88e45112b787aa864d9d3bdd7c26a61b843df6a7
Alejandro Claros <alejandrocontreras.ac83@gmail.com>
2020-12-15 15:40:18 -0600

    tooltip fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBMukomB1O3pN9yJA5bxOuEdp6RJ7BViuc",
+    authDomain: "simulationtheory-bbfe1.firebaseapp.com",
+    projectId: "simulationtheory-bbfe1",
+    storageBucket: "simulationtheory-bbfe1.appspot.com",
+    messagingSenderId: "986187015230",
+    appId: "1:986187015230:web:1f79064344a902a4e21d6f"
+  }
 };
